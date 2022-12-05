e45302b0adfd9f4793c199d15063af6bffcf9cd9
hcampos <hcampospro@gmail.com>
2022-12-05 11:23:45 +0100

    :lipstick: Add informations for face-snap component and some style

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
-  styleUrls: ['./face-snap.component.css']
+  styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
 
+export class FaceSnapComponent implements OnInit {
+  title!: string
+  description!: string
+  createDate!: Date
+  snaps!: number
+  imgUrl!: string
+
+  ngOnInit(): void {
+    this.title = "Paris"
+    this.description = "Visiter Paris !"
+    this.createDate = new Date()
+    this.snaps = 0
+    this.imgUrl = 'https://imgs.search.brave.com/8zsOTLfOSWsEC-eyQYmpZpcb-R57gCL9HTmNSUa2fh4/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5z/cTZQMDBJTDZtZWtJ/d2pIbEtmSl9BSGFF/OCZwaWQ9QXBp'
+  }
 }
