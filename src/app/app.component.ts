f86008430012e0bcf28b2ffc174a0a2b0d920827
hcampos <hcampospro@gmail.com>
2022-12-06 11:29:26 +0100

    :sparkles: Add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnapModel } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
@@ -8,31 +7,8 @@ import { FaceSnapModel } from './models/face-snap.model';
 })
 export class AppComponent implements OnInit{
   title = 'first-angular-app';
-  facesnap!: FaceSnapModel[]
 
   ngOnInit(): void {
-    this.facesnap = [{
-      title: "Paris",
-      description: "Visiter Paris !",
-      createDate: new Date(),
-      imgUrl: 'https://imgs.search.brave.com/8zsOTLfOSWsEC-eyQYmpZpcb-R57gCL9HTmNSUa2fh4/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5z/cTZQMDBJTDZtZWtJ/d2pIbEtmSl9BSGFF/OCZwaWQ9QXBp',
-      location: 'Paris',
-    },
     
-    {
-      title: "Niort",
-      description: "Visiter Niort !",
-      createDate: new Date(),
-      imgUrl: 'https://imgs.search.brave.com/knUPIwylkzkkQwrlHrNrmaHJPwQ3_J-Hkl6Kb9CuepE/rs:fit:632:225:1/g:ce/aHR0cHM6Ly90c2Uy/LmV4cGxpY2l0LmJp/bmcubmV0L3RoP2lk/PU9JUC5RYm1DYWxj/YUdXb0lOcmxIcWRD/aGFnSGFGaiZwaWQ9/QXBp',
-      location: 'Niort',
-    },
-    
-    {
-      title: "Poitiers",
-      description: "Visiter Poitiers !",
-      createDate: new Date(),
-      imgUrl: 'https://imgs.search.brave.com/9rLC5v8uvLhxYXNHkGoHw_2BVSqz7hdqsXaRHkymL8o/rs:fit:729:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5N/R1FoTmFaOTY0VTR1/WTdvZzRxTHhRSGFF/MCZwaWQ9QXBp',
-      location : 'Poitiers',
-    }]
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './components/face-snap/face-snap.component';
 import { ChangeColorDirective } from './directives/change-color.directive';
 import { registerLocaleData } from '@angular/common'
-import * as fr from '@angular/common/locales/fr'
+import * as fr from '@angular/common/locales/fr';
+import { FaceSnapListComponent } from './components/face-snap-list/face-snap-list.component';
+import { HeaderComponent } from './components/header/header.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     FaceSnapComponent,
-    ChangeColorDirective
+    ChangeColorDirective,
+    FaceSnapListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
