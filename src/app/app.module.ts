f5ba65ca5657a48f47ca6e8ac6f9838043324766
hcampos <hcampospro@gmail.com>
2022-12-06 11:07:17 +0100

    :sparkles: Try directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './components/face-snap/face-snap.component';
 import { ChangeColorDirective } from './directives/change-color.directive';
+import { registerLocaleData } from '@angular/common'
+import * as fr from '@angular/common/locales/fr'
 
 @NgModule({
   declarations: [
@@ -14,7 +16,16 @@ import { ChangeColorDirective } from './directives/change-color.directive';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'fr-FR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor() {
+    registerLocaleData(fr.default)
+  }
+}
--- src/app/directives/change-color.directive.ts
+++ src/app/directives/change-color.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appChangeColor]'
 })
 export class ChangeColorDirective {
 
+  @HostBinding('style.backgroundColor')
+  backGround = 'red'
+
+  @HostBinding('style.color')
+  color = 'black'
+
   constructor() { }
 
+  @HostListener('mouseenter')
+  mouseEnter() {
+    this.backGround = 'yellow'
+    this.color = 'salmon' 
+  }
+
+  @HostListener('mouseleave')
+  mouseLeave() {
+    this.backGround = 'red'
+    this.color = 'black'
+  }
 }
